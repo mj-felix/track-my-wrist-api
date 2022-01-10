3206414bf64b34fb1e7ab940d1c5e514e7c09e26
Maciej Szczesny <maciej@end-game.com>
2022-01-11 12:09:38 +1300

    Add GetAllWatches and GetWatchById action methods to Watch controller

--- TrackMyWristAPI/Controllers/WatchController.cs
+++ TrackMyWristAPI/Controllers/WatchController.cs
@@ -1,22 +1,30 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TrackMyWristAPI.Models;
 
 namespace TrackMyWristAPI.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/watches")]
     public class WatchController : ControllerBase
     {
-        private static Watch watch = new Watch();
+        private static List<Watch> watches = new List<Watch>{
+            new Watch{Id=1},
+            new Watch{Id=2},
+            new Watch{Id=3},
+        };
 
         [HttpGet]
-        public ActionResult<Watch> Get()
+        public ActionResult<List<Watch>> GetAllWatches()
         {
-            return Ok(watch);
+            return Ok(watches);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Watch> GetWatchById(int id)
+        {
+            return Ok(watches.FirstOrDefault(w => w.Id == id));
         }
     }
 }
