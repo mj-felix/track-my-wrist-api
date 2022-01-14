a95d838962c97b76f63fdf7ae7f0c1943fe5f5c3
Maciej Szczesny <maciej@end-game.com>
2022-01-14 13:47:35 +1300

    Implement global exception handling

--- TrackMyWristAPI/Startup.cs
+++ TrackMyWristAPI/Startup.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -92,10 +94,36 @@ namespace TrackMyWristAPI
         {
             // if (env.IsDevelopment())
             // {
-            app.UseDeveloperExceptionPage();
+            //     app.UseDeveloperExceptionPage();
+            // }
+            // else
+            // {
+            app.UseExceptionHandler(
+                options =>
+                {
+                    options.Run(
+                        async context =>
+                        {
+                            var ex = context.Features.Get<IExceptionHandlerFeature>();
+                            if (ex != null)
+                            {
+                                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                                await context.Response.WriteAsJsonAsync(
+                                    new
+                                    {
+                                        Message = ex.Error.Message,
+                                        ExceptionType = ex.Error.GetType().ToString()
+                                    }
+                                );
+                            }
+                        }
+                    );
+                }
+            );
+            // }
+
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TrackMyWristAPI v1"));
-            // }
 
             app.UseHttpsRedirection();
 
